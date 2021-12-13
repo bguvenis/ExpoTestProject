c3bd97494320df79bf786437032a8039773d1ed5
Barış Güveniş <bguvenis@gmail.com>
2021-12-13 11:37:19 +0200

    Initiail commit.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+
+import { connectToDevTools } from "react-devtools-core";
+if (__DEV__) {
+  connectToDevTools({
+    host: "localhost",
+    port: 8097,
+  });
+}
 
 export default function App() {
+  const width = useSharedValue(100);
+  const [counter, setCounter ] =useState(0);
+
+  const onPress = () => {
+    setCounter(counter+ 1);
+  }
+
+
+  const rViewStyle = useAnimatedStyle(() => {
+    return {
+      width: width.value,
+      height: 100,
+      backgroundColor: 'black'
+    }
+  })
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={rViewStyle} />
+      <Button title='animate width' onPress={() => width.value = withSpring( Math.random()*100)} />
+      <View>
+        <Text>{counter}</Text>
+        <Button title='increment' onPress={onPress}  />
+      </View>
     </View>
   );
 }
